e64b5d0c92fa489029e3ca78f9978e3afb5a192d
roshan71 <patelroshan882@gmail.com>
2022-11-22 19:36:55 +0530

    remove unwanted

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,11 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Admin from '../components/layouts/Admin'
 import Dashboard from './dashboard'
+
 export default function Home() {
   return (
   <>
-    <Dashboard/>
+    <Admin/>
   </>
   )
 }

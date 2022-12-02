0f4839aa3c3750f6d3f5528c6d4f96055b1f4028
roshan71 <patelroshan882@gmail.com>
2022-12-02 20:33:13 +0530

    update env2

--- pages/User/TempPassword.js
+++ pages/User/TempPassword.js
@@ -27,16 +27,16 @@ export async function getServerSideProps(context) {
     const o=[]
     require('dotenv').config
     const serviceAccount={
-      type:process.env.TYPE,
-      project_id:process.env.PROJECT_ID,
-      private_key_id:process.env.PRIVATE_KEY_ID ,
-      private_key:process.env.PRIVATE_KEY ,
-      client_email:process.env.CLIENT_EMAIL ,
-      client_id:process.env.CLIENT_ID,
-      auth_uri:process.env.AUTH_URI ,
-      token_uri:process.env.TOKEN_URI ,
-      auth_provider_x509_cert_url:process.env.AUTH_PROVIDER_X509_CERT_URL ,
-      client_x509_cert_url:process.env.CLIENT_CERT_URL, 
+      type:String(process.env.TYPE),
+      project_id:String(process.env.PROJECT_ID),
+      private_key_id:String(process.env.PRIVATE_KEY_ID) ,
+      private_key:String(process.env.PRIVATE_KEY) ,
+      client_email:String(process.env.CLIENT_EMAIL),
+      client_id:String(process.env.CLIENT_ID),
+      auth_uri:String(process.env.AUTH_URI) ,
+      token_uri:String(process.env.TOKEN_URI) ,
+      auth_provider_x509_cert_url:String(process.env.AUTH_PROVIDER_X509_CERT_URL) ,
+      client_x509_cert_url:String(process.env.CLIENT_CERT_URL), 
     }
     if(Object.keys(context.query).length!==0){
         
